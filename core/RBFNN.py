Add custom geodesic computation from the initial codebase, also fix rbfnn

core/RBFNN.py
@@ -39,7 +39,7 @@ class RBFNN(nn.Module):
             c_k = self.centers_rbf[k, :].reshape(-1, 1)
             S = (np.diag((points - c_k.T).T @ (points - c_k.T))/points.shape[0]).min()
             lambdas_k[k, 0] = np.sqrt(S)
-        lambdas_k = 1.25 * lambdas_k  # Increase the Sigma by a global factor to smooth the metric a bit, a=1.25
+        lambdas_k = self.a * lambdas_k  # Increase the Sigma by a global factor to smooth the metric a bit, a=1.25
         lambdas_k = 0.5 / (lambdas_k ** 2)
         lambdas_k = torch.from_numpy(lambdas_k.astype(np.float32))
         self.lambdas_k = lambdas_k

core/VAE_RBF.py
@@ -16,8 +16,8 @@ class VAE_RBF(nn.Module):
         return MU_X_eval, LOG_VAR_X_RBF, Z_ENC_eval, MU_Z_eval, LOG_VAR_Z_eval
     
     def forward_RBF(self, z):
-        LOG_VAR_X_RBF = self.RBF(z)
-        return LOG_VAR_X_RBF
+        beta = self.RBF(z)
+        return beta
     
     def forward_VAE(self, x):
         return self.VAE(x)

core/riemannian_metric.py
@@ -14,7 +14,10 @@ class Riemannian_metric:
         return np.sqrt(np.abs(np.linalg.det(M_z))).reshape(-1,1)
 
     def compute_riemannian_metric(self,z, var_rbfn):
-        N, d = z.size()
+        N, d = z.shape
+        # if z not tensor convert it to tensor
+        if not torch.is_tensor(z):
+            z = torch.Tensor(z)
         z.requires_grad_(True)  # Enable gradient computation for z
 
         # Will contain the Riemannian metric tensor at each point of the latent space
@@ -87,7 +90,7 @@ class Riemannian_metric:
         for n in range(N):
             img_plot[n] = np.sqrt(np.abs(np.linalg.det(np.squeeze(M_z[n, :, :]))))
         img_plot = img_plot.reshape(X1.shape)
-
+        
         if isLog:
             # to better vizualize small differences in the determinant
             img_plot = np.log(img_plot + 1e-10)
@@ -103,4 +106,4 @@ class Riemannian_metric:
         # Labeling the axes
         plt.xlabel('Latent Dimension 1')
         plt.ylabel('Latent Dimension 2')
-        plt.title('Heatmap of Riemannian Metric Tensor Determinant in Latent Space')
+        #plt.title('Heatmap of Riemannian Metric Tensor Determinant in Latent Space')